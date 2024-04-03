40de16e631065a96b8fe79f1d2300c34dcdf534e
Julius Muhumuza <juliusnm@udel.edu>
2024-04-03 16:05:04 -0400

    addName

--- src/App.tsx
+++ src/App.tsx
@@ -28,6 +28,9 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
+        <ul>
+          <li> Julius Muhumuza </li>
+        </ul>
         <p>
           Edit <code>src/App.tsx</code> and save to reload.
         </p>
