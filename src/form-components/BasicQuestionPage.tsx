538b20ca268ead6a6ec0bf075277a539116643be
TrevorBrennan11 <128639556+TrevorBrennan11@users.noreply.github.com>
2024-04-16 08:23:50 -0400

    Revert "Revert "Fix spacing trevor brennan11""

--- src/form-components/BasicQuestionPage.tsx
+++ src/form-components/BasicQuestionPage.tsx
@@ -2,34 +2,35 @@ import React, { useState } from "react";
 import { Form } from "react-bootstrap";
 
 export function BasicQuestionsPage(): JSX.Element {
-   
- // This is the State (Model)
- const [/*basicQuestion*/, setBasicQuestion] = useState<string>("Default");
+    // This is the State (Model)
+    const [/*basicQuestion*/, setBasicQuestion] = useState<string>("Default");
 
- // This is the Control
- function updateBasicQuestion(event: React.ChangeEvent<HTMLInputElement>) {
-     setBasicQuestion(event.target.value);
- }
+    // This is the Control
+    function updateBasicQuestion(event: React.ChangeEvent<HTMLInputElement>) {
+        setBasicQuestion(event.target.value);
+    }
 
-const [emotions, setEmotions] = useState<string[]>(["happy", "sad"]);
+    const [emotions, setEmotions] = useState<string[]>(["happy", "sad"]);
 
-// This is the Control
-function updateEmotion(event: React.ChangeEvent<HTMLInputElement>) {
-const emotion = event.target.value;
-// Check if the emotion is already present
-if (emotions.includes(emotion)) {
- // Remove the given value
-    setEmotions(emotions.filter((e) => e !== emotion));
-    } 
-    else {
-     // Append the given value
-        setEmotions([...emotions, emotion]);
+    // This is the Control
+    function updateEmotion(event: React.ChangeEvent<HTMLInputElement>) {
+        const emotion = event.target.value;
+        // Check if the emotion is already present
+        if (emotions.includes(emotion)) {
+            // Remove the given value
+            setEmotions(emotions.filter((e) => e !== emotion));
+        } else {
+            // Append the given value
+            setEmotions([...emotions, emotion]);
         }
-     }     
-return (
-            <div>
+    }     
+    return (
+        <div>
             <h1>Career Quiz Basic Questions</h1>
-            <h3>In order for us to estimate your personal Interests and Usual Style, you will first need to answer a series of questions. Read each pair of phrases below and decide which one of the two most describes you, then select the radio button next to that phrase. </h3>    
+            <h3>
+                In order for us to estimate your personal Interests and Usual Style, you will first need to answer a series of questions. 
+                Read each pair of phrases below and decide which one of the two most describes you, then select the radio button next to that phrase.
+            </h3>    
             <p>This is just a radio type question </p>
             <Form.Check
                 type="radio"
@@ -131,7 +132,6 @@ return (
                 value="Default"
             />
 
-            
             <p>Multi-Choice Question</p>
             <Form.Check
                 type="checkbox"
@@ -161,7 +161,7 @@ return (
                 onChange={updateEmotion}
             />
 
-<p>Multi-Choice Question</p>
+            <p>Multi-Choice Question</p>
             <Form.Check
                 type="checkbox"
                 id="emotion-check-happy"
@@ -190,7 +190,7 @@ return (
                 onChange={updateEmotion}
             />
 
-<p>Multi-Choice Question</p>
+            <p>Multi-Choice Question</p>
             <Form.Check
                 type="checkbox"
                 id="emotion-check-happy"
@@ -219,7 +219,7 @@ return (
                 onChange={updateEmotion}
             />
 
-<p>Multi-Choice Question</p>
+            <p>Multi-Choice Question</p>
             <Form.Check
                 type="checkbox"
                 id="emotion-check-happy"
@@ -248,7 +248,7 @@ return (
                 onChange={updateEmotion}
             />
 
-<p>Multi-Choice Question</p>
+            <p>Multi-Choice Question</p>
             <Form.Check
                 type="checkbox"
                 id="emotion-check-happy"
@@ -276,10 +276,8 @@ return (
                 checked={emotions.includes("angry")}
                 onChange={updateEmotion}
             />
-
-
         </div>
-        );
-    }
+    );
+}
     
 
