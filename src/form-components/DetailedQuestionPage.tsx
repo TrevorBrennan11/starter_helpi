e3bb75826627672549bfab34155086177b6207f2
Julius Muhumuza <juliusnm@udel.edu>
2024-04-22 17:33:33 -0400

    addedNotification

--- src/form-components/DetailedQuestionPage.tsx
+++ src/form-components/DetailedQuestionPage.tsx
@@ -1,6 +1,5 @@
 import React, { useState } from "react";
-import { Form } from "react-bootstrap";
-
+import {Form } from "react-bootstrap";
 export function DetailedQuestionsPage(): JSX.Element {
   const [answer1, setAnswer1] = useState<string> ("");
   const [answer2, setAnswer2] = useState<string> ("");
@@ -9,36 +8,48 @@ export function DetailedQuestionsPage(): JSX.Element {
   const [answer5, setAnswer5] = useState<string> ("");
   const [answer6, setAnswer6] = useState<string> ("");
   const [answer7, setAnswer7] = useState<string> ("");
+  //const answers = [false,false,false,false,false,false,false];
   //const [answer, setAnswer] = useState<string> ("");
   //const answers: string[] = [];
 
   function updateAnswer1 (event: React.ChangeEvent<HTMLInputElement>) {
     setAnswer1(event.target.value)
-    //answers.push(answer);
+    
   }
   function updateAnswer2 (event: React.ChangeEvent<HTMLInputElement>) {
     setAnswer2(event.target.value)
-    //answers.push(answer);
-  }function updateAnswer3 (event: React.ChangeEvent<HTMLInputElement>) {
+    
+  }
+  function updateAnswer3 (event: React.ChangeEvent<HTMLInputElement>) {
     setAnswer3(event.target.value)
+
     //answers.push(answer);
-  }function updateAnswer4 (event: React.ChangeEvent<HTMLInputElement>) {
+  }
+  function updateAnswer4 (event: React.ChangeEvent<HTMLInputElement>) {
     setAnswer4(event.target.value)
+    
     //answers.push(answer);
-  }function updateAnswer5 (event: React.ChangeEvent<HTMLInputElement>) {
+  }
+  function updateAnswer5 (event: React.ChangeEvent<HTMLInputElement>) {
     setAnswer5(event.target.value)
+    
     //answers.push(answer);
-  }function updateAnswer6 (event: React.ChangeEvent<HTMLInputElement>) {
+  }
+  function updateAnswer6 (event: React.ChangeEvent<HTMLInputElement>) {
     setAnswer6(event.target.value)
+    
     //answers.push(answer);
-  }function updateAnswer7 (event: React.ChangeEvent<HTMLInputElement>) {
+  }
+  function updateAnswer7 (event: React.ChangeEvent<HTMLInputElement>) {
     setAnswer7(event.target.value)
+    
     //answers.push(answer);
   }
 
   return ( 
     <div className="DetailedPage">
       <h1>Career Quiz Detailed Questions</h1>
+      
       <p>Question 1: How inclined are you to take leadership roles when working in groups?</p> 
       <Form.Group controlId="Answer 1">
         <Form.Control
@@ -60,6 +71,7 @@ export function DetailedQuestionsPage(): JSX.Element {
           value={answer3}
           onChange={updateAnswer3}/>
       </Form.Group>
+      {answer1 && answer2 && answer3 && answer4 && answer5 && answer6 && answer7 && <div> <h2>You completed the quiz, congrats!</h2></div>}
       <p>Question 4: What part of classes do you excel the most in (group work, exams, projects, etc)</p>
       <Form.Group controlId="Answer 4">
         <Form.Control
@@ -88,6 +100,7 @@ export function DetailedQuestionsPage(): JSX.Element {
           value={answer7}
           onChange={updateAnswer7}/>
       </Form.Group>
+      
     </div>
   );
 }
