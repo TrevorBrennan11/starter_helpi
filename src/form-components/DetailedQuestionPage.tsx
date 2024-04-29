39e5e050976b0b2f782f91182d65b4d51dc317c6
TrevorBrennan11 <trevor.brennan@gmail.com>
2024-04-29 11:16:58 -0400

    made questions easier to read

--- src/form-components/DetailedQuestionPage.tsx
+++ src/form-components/DetailedQuestionPage.tsx
@@ -58,49 +58,49 @@ export function DetailedQuestionsPage(): JSX.Element {
       <div className="DetailedPage">
         <h1>Career Quiz Detailed Questions</h1>
         <progress className="Progress-Bar" value={numAnswered} max={7}></progress>
-        <p>Question 1: How inclined are you to take leadership roles when working in groups?</p> 
+        <h3>Question 1: How inclined are you to take leadership roles when working in groups?</h3> 
         <Form.Group controlId="Answer 1">
           <Form.Control
             placeholder="Answer here"
             value={answers[0]}
             onChange={(e) => updateAnswer(0, e.target.value)}/>
         </Form.Group>
-        <p>Question 2: What physical environment do you prefer to be in, beyond just working?</p>
+        <h3>Question 2: What physical environment do you prefer to be in, beyond just working?</h3>
         <Form.Group controlId="Answer 2">
           <Form.Control
             placeholder="Answer here"
             value={answers[1]}
             onChange={(e) => updateAnswer(1, e.target.value)} />
         </Form.Group>
-        <p>Question 3: If you could do anything, without needing to worry about money, what would be and why?</p>
+        <h3>Question 3: If you could do anything, without needing to worry about money, what would be and why?</h3>
         <Form.Group controlId="Answer 3">
           <Form.Control
             placeholder="Answer here"
             value={answers[2]}
             onChange={(e) => updateAnswer(2, e.target.value)}/>
         </Form.Group>
-        <p>Question 4: What part of classes do you excel the most in (group work, exams, projects, etc)</p>
+        <h3>Question 4: What part of classes do you excel the most in (group work, exams, projects, etc)</h3>
         <Form.Group controlId="Answer 4">
           <Form.Control
             placeholder="Answer here"
             value={answers[3]}
             onChange={(e) => updateAnswer(3, e.target.value)}/>
         </Form.Group>
-        <p>Question 5: What was your favorite class you've taken, and why? Be specific about the class!</p>
+        <h3>Question 5: What was your favorite class you've taken, and why? Be specific about the class!</h3>
         <Form.Group controlId="Answer 5">
           <Form.Control
             placeholder="Answer here"
             value={answers[4]}
             onChange={(e) => updateAnswer(4, e.target.value)}/>
         </Form.Group>
-        <p>Question 6: What is a work environment you did not work well in at all, and would not like to return to?</p>
+        <h3>Question 6: What is a work environment you did not work well in at all, and would not like to return to?</h3>
         <Form.Group controlId="Answer 6">
           <Form.Control
             placeholder="Answer here"
             value={answers[5]}
             onChange={(e) => updateAnswer(5, e.target.value)}/>
         </Form.Group>
-        <p>Question 7: What aspect of working are you most looking forward to in the future?</p>
+        <h3>Question 7: What aspect of working are you most looking forward to in the future?</h3>
         <Form.Group controlId="Answer 7">
           <Form.Control
             placeholder="Answer here"
