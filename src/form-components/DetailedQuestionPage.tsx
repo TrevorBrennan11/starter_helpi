e2e5ccb4f28c049d910179af7a4d6dbdcb3486b9
TrevorBrennan11 <trevor.brennan@gmail.com>
2024-04-29 11:33:37 -0400

    made text entires bigger and resizeable

--- src/form-components/DetailedQuestionPage.tsx
+++ src/form-components/DetailedQuestionPage.tsx
@@ -59,54 +59,47 @@ export function DetailedQuestionsPage(): JSX.Element {
         <h1>Career Quiz Detailed Questions</h1>
         <progress className="Progress-Bar" value={numAnswered} max={7}></progress>
         <h3>Question 1: How inclined are you to take leadership roles when working in groups?</h3> 
-        <Form.Group controlId="Answer 1">
-          <Form.Control
+        <textarea 
+            style={{width: "75%"}}
             placeholder="Answer here"
             value={answers[0]}
             onChange={(e) => updateAnswer(0, e.target.value)}/>
-        </Form.Group>
         <h3>Question 2: What physical environment do you prefer to be in, beyond just working?</h3>
-        <Form.Group controlId="Answer 2">
-          <Form.Control
+        <textarea 
+            style={{width: "75%"}}
             placeholder="Answer here"
             value={answers[1]}
-            onChange={(e) => updateAnswer(1, e.target.value)} />
-        </Form.Group>
+            onChange={(e) => updateAnswer(1, e.target.value)}/>
         <h3>Question 3: If you could do anything, without needing to worry about money, what would be and why?</h3>
-        <Form.Group controlId="Answer 3">
-          <Form.Control
+        <textarea 
+            style={{width: "75%"}}
             placeholder="Answer here"
             value={answers[2]}
             onChange={(e) => updateAnswer(2, e.target.value)}/>
-        </Form.Group>
         <h3>Question 4: What part of classes do you excel the most in (group work, exams, projects, etc)</h3>
-        <Form.Group controlId="Answer 4">
-          <Form.Control
+        <textarea 
+            style={{width: "75%"}}
             placeholder="Answer here"
             value={answers[3]}
             onChange={(e) => updateAnswer(3, e.target.value)}/>
-        </Form.Group>
         <h3>Question 5: What was your favorite class you've taken, and why? Be specific about the class!</h3>
-        <Form.Group controlId="Answer 5">
-          <Form.Control
+        <textarea 
+            style={{width: "75%"}}
             placeholder="Answer here"
             value={answers[4]}
             onChange={(e) => updateAnswer(4, e.target.value)}/>
-        </Form.Group>
         <h3>Question 6: What is a work environment you did not work well in at all, and would not like to return to?</h3>
-        <Form.Group controlId="Answer 6">
-          <Form.Control
+        <textarea 
+            style={{width: "75%"}}
             placeholder="Answer here"
             value={answers[5]}
             onChange={(e) => updateAnswer(5, e.target.value)}/>
-        </Form.Group>
         <h3>Question 7: What aspect of working are you most looking forward to in the future?</h3>
-        <Form.Group controlId="Answer 7">
-          <Form.Control
+        <textarea 
+            style={{width: "75%"}}
             placeholder="Answer here"
             value={answers[6]}
             onChange={(e) => updateAnswer(6, e.target.value)}/>
-        </Form.Group> 
         {answers[0] && answers[1] && answers[2] && answers[3] && answers[4] && answers[5] && answers[6] && <div>
           <Col as={Button} onClick={showMyResults}>Get Results</Col>
           <span></span><h2>Congrats you completed the quiz!</h2>
