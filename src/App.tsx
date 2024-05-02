9c1c0fc69fd6f4832b16ef5617c80fb83262d979
TrevorBrennan11 <trevor.brennan@gmail.com>
2024-05-02 15:47:19 -0400

    fixed unused imports

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
 import React, { useState } from 'react';
 import './App.css';
-import { Button, Col, Container, Form } from 'react-bootstrap';
+import { Button, Form } from 'react-bootstrap';
 import { BasicQuestionsPage } from './form-components/BasicQuestionPage';
 import { DetailedQuestionsPage } from './form-components/DetailedQuestionPage';
 import { HomePage } from './form-components/HomePage';
